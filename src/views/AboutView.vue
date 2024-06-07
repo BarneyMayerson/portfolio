5322bf4f8a8d646946e7d56f7e10f9515ec39874
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-06-07 09:39:12 +0300

    wip

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container mx-auto mt-8">
+    <div class="container mx-auto mt-8 px-4 lg:px-8">
       <h1 class="text-3xl font-bold">This is About page</h1>
     </div>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="container mx-auto mt-8">
+  <div class="container mx-auto mt-8 px-4 lg:px-8">
     <h1 class="text-3xl font-bold">Here is the Home page</h1>
   </div>
 </template>
