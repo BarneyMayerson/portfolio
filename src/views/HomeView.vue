c9afa3d4c3af33543d3a599f7d5f3910575fa40a
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-06-18 14:52:18 +0300

    Theming borders color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,15 @@ const { theme } = useThemes(); // needs to init the theme was saved in the local
 
 const displaySettings = ref(false);
 
-const { bgColorClasses, textColorClasses } = useColorSchemes();
+const { bgColorClasses, borderColorClasses, textColorClasses } =
+  useColorSchemes();
 
 const colorScheme = useColorSchemeStore();
 const primaryBgColorClass = computed(
   () => bgColorClasses[colorScheme.primaryColor][colorScheme.primaryShade]
+);
+const primaryBorderColorClass = computed(
+  () => borderColorClasses[colorScheme.primaryColor][colorScheme.primaryShade]
 );
 const primaryColorClass = computed(
   () => textColorClasses[colorScheme.primaryColor][colorScheme.primaryShade]
@@ -50,7 +54,8 @@ watch(primaryColor, () => {
     <LinkParticles class="h-screen" :primaryColor :key="renderKey" />
     <div class="absolute left-1/4 lg:left-1/3 top-1/3 w-1/2 lg:w-1/3">
       <div
-        class="bg-black/40 w-full md:mx-0 md:p-8 rounded border p-4 text-center text-white border-red-500"
+        class="bg-black/20 w-full md:mx-0 md:p-8 rounded border p-4 text-center"
+        :class="`${primaryBorderColorClass} ${primaryColorClass}`"
       >
         <h1 class="text-2xl sm:text-3xl md:text-4xl">Barney Mayerson</h1>
         <h2 class="sm:text-1xl text-lg md:text-2xl">Web App Developer</h2>
