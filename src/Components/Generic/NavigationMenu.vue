324abf1977e21ee67eb4adacb58a6fc6808e5af3
Barney Mayerson <mr.barney.mayerson@gmail.com>
2024-06-25 15:30:54 +0300

    Drop unused (wip)

--- src/Components/Generic/NavigationMenu.vue
+++ src/Components/Generic/NavigationMenu.vue
@@ -1,19 +1,9 @@
 <script setup>
-import { computed } from "vue";
 import NavLink from "@/Components/Generic/NavLink.vue";
-import { useColorSchemeStore } from "@/stores/colorScheme";
-import { useColorSchemes } from "@/Composables/useColorSchemes";
 
 defineProps({
-  currentRouteName: String,
   currentRouteHash: String,
 });
-
-const colorScheme = useColorSchemeStore();
-const { textColorClasses } = useColorSchemes();
-const primaryColorClass = computed(
-  () => textColorClasses[colorScheme.primaryColor][colorScheme.primaryShade]
-);
 </script>
 
 <template>
@@ -23,31 +13,21 @@ const primaryColorClass = computed(
     <div class="container mx-auto px-4 lg:px-8">
       <div class="h-16 flex items-center justify-between">
         <div class="flex items-center">
-          <div class="flex items-center">
-            <svg
-              width="54"
-              height="54"
-              viewBox="0 0 54 54"
-              class="mr-2 h-8 w-8 fill-current"
-              :class="primaryColorClass"
-            >
-              <path
-                d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
+          <a href="/">
+            <div class="flex items-center space-x-3">
+              <img
+                src="https://avatars.githubusercontent.com/u/120561976?v=4"
+                class="w-10 h-10 rounded-full"
               />
-            </svg>
-            <span
-              class="text-xl font-semibold tracking-tight"
-              :class="primaryColorClass"
-            >
-              Barney Mayerson
-            </span>
-          </div>
+              <span
+                class="text-xl font-semibold tracking-tight text-gray-800 dark:text-gray-100"
+              >
+                Barney Mayerson
+              </span>
+            </div>
+          </a>
           <!-- Navigation Links -->
           <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-            <NavLink :to="{ name: 'home' }" :active="!currentRouteHash">
-              Home
-            </NavLink>
-
             <NavLink
               :to="{ name: 'home', hash: '#about-me' }"
               :active="currentRouteHash === '#about-me'"
@@ -59,10 +39,6 @@ const primaryColorClass = computed(
               :active="currentRouteHash === '#projects'"
             >
               Projects
-            </NavLink>
-
-            <NavLink to="about-me" :active="currentRouteName === 'about'">
-              About Me
             </NavLink>
           </div>
         </div>
